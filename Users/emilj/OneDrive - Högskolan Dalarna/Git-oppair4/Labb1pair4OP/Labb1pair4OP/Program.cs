d55ba868f89454d5dda9fc23216717b6f704905e
emilgrupp16 <emil.johansson62@hotmail.com>
2024-11-20 11:18:27 +0100

    yo

---
+++ Users/emilj/OneDrive - Högskolan Dalarna/Git-oppair4/Labb1pair4OP/Labb1pair4OP/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Labb1pair4OP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            asdasd.hejsan
+        }
+    }
+}
